901061a26b4a8971e7711283f33ae928ce18ed0d
dcunhamaciel <dcunhamaciel@gmail.com>
2025-02-05 21:02:30 -0300

    Changing User-Agent

--- 06. Building Crawlers with Scrapy/crawler/crawler/spiders/transcripts.py
+++ 06. Building Crawlers with Scrapy/crawler/crawler/spiders/transcripts.py
@@ -5,12 +5,24 @@ from scrapy.spiders import CrawlSpider, Rule
 class TranscriptsSpider(CrawlSpider):
     name = "transcripts"
     allowed_domains = ["subslikescript.com"]
-    start_urls = ["https://subslikescript.com/movies_letter-X"]
+    #start_urls = ["https://subslikescript.com/movies_letter-X"]
+
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36'
 
     rules = (
-        Rule(LinkExtractor(restrict_xpaths='//ul[@class="scripts-list"]/li/a'), callback='parse_item', follow=True),
-        Rule(LinkExtractor(restrict_xpaths='(//a[@rel="next"])[1]'))
+        Rule(LinkExtractor(restrict_xpaths='//ul[@class="scripts-list"]/li/a'), callback='parse_item', follow=True, process_request='set_user_agent'),
+        Rule(LinkExtractor(restrict_xpaths='(//a[@rel="next"])[1]'), process_request='set_user_agent')
     )
+
+    def start_requests(self):
+        yield scrapy.Request(
+            url='https://subslikescript.com/movies_letter-X',
+            headers={'User-Agent': self.user_agent}
+        )
+
+    def set_user_agent(self, request, spider):
+        request.headers['User-Agent'] = self.user_agent
+        return request
 
     def parse_item(self, response):
         article = response.xpath('//article[@class="main-article"]')
@@ -22,5 +34,6 @@ class TranscriptsSpider(CrawlSpider):
             'title': title,
             'plot': plot,
             #'transcript': transcript,
-            'url': response.url
+            'url': response.url,
+            'user-agent': response.request.headers['User-Agent']
         }
